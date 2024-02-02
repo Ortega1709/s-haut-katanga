659a59a2a497deac89d045d159a13aeb79b59d59
ortega <kabweortega@gmail.com>
2024-02-02 16:18:16 +0200

    Feat: Add haut-katanga server controller

---
+++ src/main/java/com/ortega/shautkatanga/SHautKatangaApplication.java
@@ -0,0 +1,13 @@
+package com.ortega.shautkatanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SHautKatangaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SHautKatangaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ortega/shautkatanga/SHautKatangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ortega.shautkatanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SHautKatangaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
