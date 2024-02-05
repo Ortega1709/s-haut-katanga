07d36b7db8bc88b29eb76fc52048cd2853a7520f
ortega <kabweortega@gmail.com>
2024-02-05 08:29:01 +0200

    Build: Add spring cloud routing dependency

--- src/main/java/com/ortega/shautkatanga/SHautKatangaController.java
+++ src/main/java/com/ortega/shautkatanga/SHautKatangaController.java
@@ -1,4 +1,4 @@
-package com.ortega.shautkatanga.controllers;
+package com.ortega.shautkatanga;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
